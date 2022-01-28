3533d4aedb85467a72cdaab4c60581099ab1b943
Peter <47712814+PeterPavlovIvanov@users.noreply.github.com>
2022-01-28 16:40:30 +0200

    TEST commit

--- src/main/java/com/models/BaseEntity.java
+++
@@ -1,4 +0,0 @@
-package com.models;
-// base abstract entity
-public abstract class BaseEntity {
-}
---
+++ src/main/java/com/models/Category.java
@@ -0,0 +1,28 @@
+package com.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+
+    private String categoryName;
+}
