8b9c6f39b00bec6ec3e43aa08179df5cfd1d16d3
Stefan Genev <stefangenev@abv.bg>
2022-01-28 15:58:29 +0200

    Initial project commit

---
+++ src/test/java/com/foodstore/Food/Store/FoodStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodstore.Food.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
