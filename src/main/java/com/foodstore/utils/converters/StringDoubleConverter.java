7634b29ae958f72380ab19708dcd221ee5d5d058
Stefan Genev <stefangenev@abv.bg>
2022-03-03 21:26:40 +0200

    Changed double formatter locale usage

--- src/main/java/com/foodstore/utils/converters/StringDoubleConverter.java
+++ src/main/java/com/foodstore/utils/converters/StringDoubleConverter.java
@@ -2,6 +2,9 @@ package com.foodstore.utils.converters;
 
 import javafx.util.StringConverter;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 // Клас за трансформация от double към низ и обратно
 
 public class StringDoubleConverter extends StringConverter<Double> {
@@ -16,6 +19,10 @@ public class StringDoubleConverter extends StringConverter<Double> {
 
     @Override
     public String toString(Double d) {
-        return String.format("%.2f", d);
+        NumberFormat formatter = NumberFormat.getInstance(Locale.getDefault());
+        formatter.setMaximumFractionDigits(2);
+        formatter.setMinimumFractionDigits(2);
+
+        return formatter.format(d);
     }
 }
